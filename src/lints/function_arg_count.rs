39f68abe77edbdb34fac5f6d9ac803765e7f6ac4
Sean Heelan <sean.heelan@gmail.com>
2015-07-09 17:21:33 +0100

    Differentiate between closures and other types of functions when deciding how many arguments is acceptable

--- src/lints/function_arg_count.rs
+++ src/lints/function_arg_count.rs
@@ -5,20 +5,28 @@ use syntax::codemap::Span;
 use syntax::visit::FnKind;
 use rustc::lint::{Context, LintArray, LintPass};
 
-const MAX_FN_ARG_LIST_LEN: usize = 6;
+const MAX_ARGS_DEFAULT: usize = 6;
+const MAX_ARGS_FOR_CLOSURE: usize = 4;
 
 declare_lint!(FN_ARG_LIST_LENGTH, Warn, "Warn about long argument lists");
 
 pub struct Pass;
+
+fn get_max_args_allowed(kind: &FnKind) -> usize {
+    match kind {
+        &FnKind::FkFnBlock => MAX_ARGS_FOR_CLOSURE,
+        _ => MAX_ARGS_DEFAULT
+    }
+}
 
 impl LintPass for Pass {
     fn get_lints(&self) -> LintArray {
         lint_array!(FN_ARG_LIST_LENGTH)
     }
 
-    fn check_fn(&mut self, cx: &Context, _: FnKind, decl: &FnDecl, _: &Block,
+    fn check_fn(&mut self, cx: &Context, kind: FnKind, decl: &FnDecl, _: &Block,
                 span: Span, _: NodeId) {
-        if decl.inputs.len() > MAX_FN_ARG_LIST_LEN {
+        if decl.inputs.len() > get_max_args_allowed(&kind) {
             cx.span_lint(FN_ARG_LIST_LENGTH, span, 
                             "function has an excessive number of arguments");
         }
